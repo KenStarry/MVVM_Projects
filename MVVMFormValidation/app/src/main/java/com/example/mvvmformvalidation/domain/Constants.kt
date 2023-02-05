77a3e62dcb03067e5a73294dc0c2abc0db99bb45
KenStarry <starrycodes@gmail.com>
2023-02-05 14:18:32 +0300

    Validate password

--- MVVMFormValidation/app/src/main/java/com/example/mvvmformvalidation/domain/Constants.kt
+++ MVVMFormValidation/app/src/main/java/com/example/mvvmformvalidation/domain/Constants.kt
@@ -2,6 +2,14 @@ package com.example.mvvmformvalidation.domain
 
 object Constants {
 
+    //  email validation
     const val BLANK_EMAIL_ERROR = "Email can't be blank"
     const val INVALID_EMAIL_ERROR = "That's not a valid email"
+
+    //  password validation
+    const val PASSWORD_LENGTH = 8
+    const val PASSWORD_LENGTH_ERROR =
+        "The password needs to contain at least $PASSWORD_LENGTH characters"
+    const val PASSWORD_LETTERS_ERROR =
+        "The password needs to contain at least one letter and digit"
 }
---
+++ MVVMFormValidation/app/src/main/java/com/example/mvvmformvalidation/domain/use_case/ValidatePassword.kt
@@ -0,0 +1,27 @@
+package com.example.mvvmformvalidation.domain.use_case
+
+import android.util.Patterns
+import com.example.mvvmformvalidation.domain.Constants
+import com.example.mvvmformvalidation.domain.model.ValidationResult
+
+class ValidatePassword {
+
+    fun execute(
+        password: String
+    ): ValidationResult {
+
+        if (password.length < Constants.PASSWORD_LENGTH) {
+            return ValidationResult.Error(Constants.PASSWORD_LENGTH_ERROR)
+        }
+
+        val containsLettersAndDigits = password.any { it.isDigit() } &&
+                password.any { it.isLetter() }
+
+        //  check if password contains letters and digits
+        if (!containsLettersAndDigits) {
+            return ValidationResult.Error(Constants.PASSWORD_LETTERS_ERROR)
+        }
+
+        return ValidationResult.Success
+    }
+}
